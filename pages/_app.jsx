1c7285816510b6ffc2de986ed2f9779c4b3ff980
Ayden Ashwill <ayden3ashwill@gmail.com>
2026-02-19 00:57:35 -0600

    Remove Teams chat widget; clean founder-only contact and leadership

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,6 @@
 // pages/_app.jsx
 import "../styles/globals.css";
 import Head from "next/head";
-import Script from "next/script";
 import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
@@ -12,19 +11,10 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      {/* Microsoft Teams Customer Chat Widget */}
-      <Script
-        src="https://res.public.onecdn.static.microsoft/customerconnect/v1/7dttl/init.js"
-        strategy="afterInteractive"
-        id="chatbot"
-        environmentId="d3ea33e1-d6b8-e6b3-a612-c30d2f7e8602"
-        region="unitedstates"
-        crossOrigin="anonymous"
-      />
-
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </>
   );
 }
+
